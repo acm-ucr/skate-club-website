088eda6cb41818fc25357621a5cb5b1344b2150c
javi <javi@Javis-MacBook-Pro-3.local>
2024-10-06 02:20:22 -0700

    fix lint

--- src/components/about/AboutUsText.tsx
+++ src/components/about/AboutUsText.tsx
@@ -11,7 +11,6 @@ const AboutUsText = () => {
 export default AboutUsText;
 
 // Use this text instead
-
 /*Skateboarding club at UC Riverside is an official, university-recognized student organization that has existed for nearly 10 years. Its main purpose is fairly obvious: to foster a skate community at UC Riverside! One long-lasting feature of Skateclub is its extremely open-door policy to anyone interested in joining. Beginners are welcome, no fees are mandatory, and there are never any pressures to perform certain duties for the club – just come, skate, and connect!
 
 Historically, Skateclub has proven to be a unique entity because it allows students from any year or major to interact with one another without any barriers. Aside from the “show up whenever and skate” attitude, Skateclub also offers occasional road trips, various events around campus, and even filming for feature skate films.
