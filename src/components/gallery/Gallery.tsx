c589935ca476d698ae3e0c204cd3112e5561c647
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-07 11:14:39 -0700

    Created styles.css for Gallery component to replicate typography properties from the Figma design.

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import "./styles.css"; // Make sure to import your CSS file
 
 const Gallery = () => {
   return (
-    <div className="flex w-full flex-row justify-center text-3xl">Gallery</div>
+    <div className="gallery-text flex w-full flex-row justify-center">
+      Gallery
+    </div>
   );
 };
 
