d0cc381e37a7e7f240e76df7a25d39c451adf86f
Risa Onishi <risaonishi24@gmail.com>
2024-10-10 16:55:34 -0700

    Added Meet The Board Title

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,11 @@
 import MeetTheBoard from "@/components/board/MeetTheBoard";
 
 const page = () => {
-  return <div className="flex w-full flex-col gap-10 bg-black">
-    <MeetTheBoard />
-    </div>;
+  return (
+    <div className="flex w-full flex-col gap-10 bg-black">
+      <MeetTheBoard />
+    </div>
+  );
 };
 
 export default page;
--- src/components/board/MeetTheBoard.tsx
+++ src/components/board/MeetTheBoard.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const MeetTheBoard = () => {
   return (
     <div className="flex w-full flex-row justify-center text-3xl">
-      <h1 className="font-orbitron text-8xl text-white">
-        Meet The Board
-      </h1>
+      <h1 className="font-orbitron text-8xl text-white">Meet The Board</h1>
     </div>
   );
 };
