bfc564723e3587dc714c735f768a2d4a481902b5
Risa Onishi <risaonishi24@gmail.com>
2024-10-10 16:53:58 -0700

    Created Meet The Board Title

--- src/components/board/MeetTheBoard.tsx
+++ src/components/board/MeetTheBoard.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const MeetTheBoard = () => {
   return (
     <div className="flex w-full flex-row justify-center text-3xl">
-      Meet The Board
+      <h1 className="font-orbitron text-8xl text-white">
+        Meet The Board
+      </h1>
     </div>
   );
 };
