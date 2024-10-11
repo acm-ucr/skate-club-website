35aa1365af5ab169066c5c4b875f0813850cd6b3
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-11 14:00:21 -0700

    Added gallery page and GalleryHeader component

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,15 @@
+import Gallery from "@/components/gallery/Gallery";
+
 const page = () => {
   return (
-    <div className="flex w-full flex-col gap-10 font-normal">Gallery Page</div>
+    <>
+      <div className="flex w-full flex-col gap-10 font-normal">
+        Gallery Page
+      </div>
+      <div>
+        <Gallery />
+      </div>
+    </>
   );
 };
 
