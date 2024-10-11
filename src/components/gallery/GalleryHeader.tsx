e94f5db217ab82ba6e39c5c9febf2c40e7e5b549
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-11 14:58:02 -0700

    Ran prettier format

--- src/components/gallery/GalleryHeader.tsx
+++ src/components/gallery/GalleryHeader.tsx
@@ -1,6 +1,6 @@
 const GalleryHeader = () => {
   return (
-    <div className="flex w-full flex-row justify-center text-left font-banco text-[108px] font-medium leading-[103.25px]">
+    <div className="font-banco flex w-full flex-row justify-center text-left text-[108px] font-medium leading-[103.25px]">
       Gallery
     </div>
   );
