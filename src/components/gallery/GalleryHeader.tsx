66cc681c5c38551001937317449795ef594151ad
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-17 18:39:06 -0700

    GalleryHeader component updated: 1. Removed 'leading'. 2. Changed to text-white. 3. Changed 'text-[108px]' to 'text-8xl'

--- src/components/gallery/GalleryHeader.tsx
+++ src/components/gallery/GalleryHeader.tsx
@@ -1,6 +1,6 @@
 const GalleryHeader = () => {
   return (
-    <div className="font-banco flex w-full flex-row justify-center text-left text-[108px] font-medium leading-[103.25px]">
+    <div className="font-banco flex w-full flex-row justify-center text-left text-8xl font-medium text-white">
       Gallery
     </div>
   );
