24679d1bb948ae976d35c9caf36df85a537737e7
Risa Onishi <risaonishi24@gmail.com>
2024-10-17 22:46:01 -0700

    Fixed comments from Javi

--- src/components/board/MeetTheBoard.tsx
+++ src/components/board/MeetTheBoard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MeetTheBoard = () => {
   return (
-    <div className="flex w-full flex-row justify-center text-3xl">
+    <div className="flex w-full justify-center">
       <h1 className="font-orbitron text-8xl text-white">Meet The Board</h1>
     </div>
   );
