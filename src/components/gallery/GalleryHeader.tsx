11c0b791314de5f96a5c0220cc5b84e1c5f680ed
javi <javi@Javis-MacBook-Pro-3.local>
2024-10-21 13:13:36 -0700

    Format

--- src/components/gallery/GalleryHeader.tsx
+++ src/components/gallery/GalleryHeader.tsx
@@ -1,6 +1,6 @@
 const GalleryHeader = () => {
   return (
-    <div className="font-banco flex w-full flex-row justify-center text-left text-8xl font-medium text-white">
+    <div className="flex w-full flex-row justify-center text-left font-banco text-8xl font-medium text-white">
       Gallery
     </div>
   );
