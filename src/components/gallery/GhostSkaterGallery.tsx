2f55736d18523a47ff62bfda948aefc1b23d2da4
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-25 14:17:42 -0700

    Moved Ghost and GalleryRamp.svg to outside public folder. Adjusted GhostSkaterGallery to use GalleryRamp.svg instead of Ramp.svg

--- src/components/gallery/GhostSkaterGallery.tsx
+++ src/components/gallery/GhostSkaterGallery.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Image from "next/image";
-import Ghost from "../../public/images/Ghost.svg";
-import ramp from "../../public/images/Ramp.svg";
+import Ghost from "@/public/Ghost.svg";
+import GalleryRamp from "@/public/GalleryRamp.svg";
 
 const GhostSkaterGallery = () => {
   return (
     <div className="relative flex w-full">
-      <Image src={ramp} alt="skating ramp" className="w-full" />
+      <Image src={GalleryRamp} alt="skating ramp" className="w-full" />
       <Image
         src={Ghost}
         alt="skating ghost"
-        className="rotate-315 absolute left-1/4 w-1/6"
+        className="rotate-315 absolute -top-1/2 left-1/4 w-1/6"
       />
     </div>
   );
