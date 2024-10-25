4b10769ae3fdfecf330a744de656ff5e8eab6cf9
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-25 10:50:32 -0700

    Added Ghost.svg , Edited GhostSkaterGallery.tsx.

--- src/components/gallery/GhostSkaterGallery.tsx
+++ src/components/gallery/GhostSkaterGallery.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import Ghost from "../../public/images/Ghost.svg";
+import ramp from "../../public/images/Ramp.svg";
 
 const GhostSkaterGallery = () => {
-  return <div className="flex w-full justify-center"></div>;
+  return (
+    <div className="flex w-full justify-center">
+      <div className="relative">
+        <Image
+          src={Ghost}
+          alt="skating ghost"
+          className="rotate-315 absolute -translate-y-10 translate-x-3/4"
+        />
+        <Image
+          src={ramp}
+          alt="skating ramp"
+          className="h-full w-full object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GhostSkaterGallery;
