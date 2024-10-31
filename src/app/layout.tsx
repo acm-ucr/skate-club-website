8f7e32f1c8791f6b83770d93901397c595534f3e
CruzL092 <cruzbusiness760@gmail.com>
2024-10-31 15:07:47 -0700

    Added Unfinished NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import {
 } from "next/font/google";
 import localFont from "next/font/local";
 import { ReactQueryClientProvider } from "@/utils/react-query";
+import Navbar from "@/components/Navbar";
 
 const banco = localFont({
   src: "../public/fonts/Banco-Regular.ttf",
@@ -52,7 +53,10 @@ export default function RootLayout({ children }: LayoutProps) {
       <body
         className={`${banco.variable} ${orbitron.variable} ${bangers.variable} ${balooThambi.variable} ${balooTamma.variable} ${inter.className} flex flex-col justify-between bg-black`}
       >
-        <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+        <Navbar />
+        <div className="flex w-full flex-col items-center justify-center">
+          <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+        </div>
       </body>
     </html>
   );
