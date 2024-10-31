b1dd736ac370c055988f68bfc8c86a9a12756ab0
Alton Qian <qian.alton@gmail.com>
2024-10-31 16:48:57 -0700

    Created Skeleton Skating image for Calendar/events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,12 @@
+import SkeletonSkatingCalendar from "@/components/calendar/SkeletonSkatingCalendar";
+
 const page = () => {
-  return <div className="flex w-full flex-col gap-10">Events Page</div>;
+  return (
+    <div className="flex w-full flex-col gap-10">
+      <SkeletonSkatingCalendar />
+      Events Page
+    </div>
+  );
 };
 
 export default page;
--- src/components/calendar/SkeletonSkatingCalendar.tsx
+++ src/components/calendar/SkeletonSkatingCalendar.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import CalendarRamp from "../../../public/assets/CalendarRamp.svg";
+import CalendarSkater from "../../../public/assets/CalendarSkeleton.svg";
 
 const SkeletonSkaterCalendar = () => {
-  return <div className="flex w-full justify-center"></div>;
+  return (
+    <div className="flex justify-center">
+      <Image
+        src={CalendarRamp}
+        alt="skating ramp"
+        className="absolute bottom-0 w-full object-center"
+      />
+
+      <Image
+        src={CalendarSkater}
+        alt="skating skeleton"
+        className="absolute bottom-56 left-1/2 z-10 size-1/2 -translate-x-1/2 transform object-center"
+      />
+    </div>
+  );
 };
 
 export default SkeletonSkaterCalendar;
