abff3b901f1c016affd211f9716968d2a0b06b16
Usman Fawaz <afawa001@ucr.edu>
2024-11-06 12:27:50 -0800

    Ran prettier

--- src/components/home/JoinNow.tsx
+++ src/components/home/JoinNow.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 interface JoinNowProps {
-    label: string;
-    onClick: () => void;
+  label: string;
+  onClick: () => void;
 }
 
 const JoinNow = ({ label, onClick }: JoinNowProps) => {
-    return (
-        <button
-            className="bg-white text-black font-bold italic py-2 px-6 rounded-full hover:bg-gray-100 transition duration-200"
-            style={{ fontFamily: 'fantasy' }}
-            onClick={onClick}
-        >
-            {label}
-        </button>
-    );
+  return (
+    <button
+      className="rounded-full bg-white px-6 py-2 font-bold italic text-black transition duration-200 hover:bg-gray-100"
+      style={{ fontFamily: "fantasy" }}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default JoinNow;
