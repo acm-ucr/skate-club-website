b09f6578b0a813a5e98a4e4482e8d1957c302eab
CruzL092 <cruzbusiness760@gmail.com>
2024-11-07 12:16:09 -0800

    Formatted Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,11 @@ import Navbar from "react-bootstrap/Navbar";
 import SkateLogo from "../public/Skate_LOGO.webp";
 import Nav from "react-bootstrap/Nav";
 import Link from "next/link";
+import { items } from "../data/Nav.js";
 //import { FaBars } from "react-icons/fa";
 
 const Navigation = () => {
-  const [setSelected] = useState("");
+  const [selected, setSelected] = useState("");
 
   return (
     <Navbar
@@ -27,7 +28,7 @@ const Navigation = () => {
           <Image
             src={SkateLogo}
             alt="Skate-Logo"
-            width={250}
+            width={300}
             height={100}
             className="m-3"
           />
@@ -39,41 +40,23 @@ const Navigation = () => {
       </Navbar.Toggle>
       <Navbar.Collapse
         id="basic-navbar-nav"
-        className="items-center justify-center"
+        className="w-screen items-center justify-center"
       >
-        <Nav className="flex w-full items-center justify-center gap-10 pr-5 text-center font-banco text-3xl no-underline">
-          <Nav.Link
-            as={Link}
-            href="/"
-            className="hover: text-white underline"
-            onClick={() => setSelected("BOARD")}
-          >
-            BOARD
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            href="/about"
-            className="text-white hover:underline"
-            onClick={() => setSelected("ABOUT US")}
-          >
-            ABOUT US
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            href="/gallery"
-            className="text-white hover:underline"
-            onClick={() => setSelected("GALLERY")}
-          >
-            GALLERY
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            href="/calendar"
-            className="text-white hover:underline"
-            onClick={() => setSelected("CALENDAR")}
-          >
-            CALENDAR
-          </Nav.Link>
+        <Nav className="flex items-center gap-10 font-banco no-underline">
+          {items.map((item, index) => (
+            <Nav.Link
+              as={Link}
+              key={index}
+              href={item.link}
+              style={{ fontSize: "3rem" }}
+              onClick={() => setSelected(item.name)}
+              className={`font-banco text-sm font-light tracking-widest text-white hover:underline lg:text-3xl ${
+                selected === item.name ? "underline" : "no-underline"
+              }`}
+            >
+              {item.name}
+            </Nav.Link>
+          ))}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
@@ -81,60 +64,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-
-// const Navigation = () => {
-//     const [selected, setSelected] = useState("");
-
-//     return (
-//         <Navbar
-//         collapseOnSelect
-//         sticky= "top"
-//         expand= "lg"
-//         className="w-full md:min-h-[7vh] lg:min-h-[9vh] bg-black flex justify-between items-center p-3"
-//         >
-//        <Navbar.Brand className = "p-0">
-//          <Link
-//          eventKey = "1"
-//          className="p-0 flex items-center"
-//          href="/"
-//          onClick={() => setSelected("")}
-//          >
-//           <Image src={SkateLogo} alt="Skate-Logo"  className  ="w-2/3 h-1 w-1 m-3"/>
-//          </Link>
-//        </Navbar.Brand>
-//        <Navbar.Toggle
-//          className="list-unstyled !text-transparent border-0"
-//          aria-controls="basic-navbar-nav"
-//        >
-//         <FaBars className= "text-white text-xl" />
-//        </Navbar.Toggle>
-//        <Navbar.Collapse
-//          id = "basic-navbar-nav"
-//          className ="items-center lg:justify-end justify-center flex"
-//          >
-
-//           <Nav className = "w-full no-underline font-normal flex items-center text-center text-sm lg:text-base justify-end= pr-5 gap-10">
-//             <p className = "text-white tracking-widest font-banco font-light hover:underline"> Hello What do I do Like acutally help wtf </p>
-
-//             </Nav>"
-//          {/* <Nav className = "w-full no-underline font-normal flex items-center text-center text-sm lg:text-base justify-end pr-5 gap-10">
-//             {items.map((item,index) => (
-//               <Nav.Link
-//                 as = {Link}
-//                 key = {index}
-//                 href={item.link}
-//                 onClick={() => setSelected(item.name)}
-//                 className= {`text-black tracking-widest font-banco font-light hover:underline ${
-//                     selected === item.name? "underline" : "no-underline"
-//                 }`}
-//                 >
-//                 {item.name}
-//               </Nav.Link>
-//             ))}
-//          </Nav> */}
-//          </Navbar.Collapse>
-//        </Navbar>
-//         );
-//     };
-
-//     export default Navigation;
---
+++ src/data/Nav.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    name: "BOARD",
+    link: "/",
+  },
+  {
+    name: "ABOUT US",
+    link: "/about ",
+  },
+  {
+    name: "GALLERY",
+    link: "gallery",
+  },
+  {
+    name: "CALENDAR",
+    link: "/calendar",
+  },
+];
