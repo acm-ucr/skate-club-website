5559b2e569385cf9b882747d596006d5640713e3
Alton Qian <qian.alton@gmail.com>
2024-11-07 16:59:46 -0800

    Made the images for Calendar page responsive

--- src/components/calendar/SkeletonSkatingCalendar.tsx
+++ src/components/calendar/SkeletonSkatingCalendar.tsx
@@ -15,7 +15,7 @@ const SkeletonSkaterCalendar = () => {
       <Image
         src={CalendarSkater}
         alt="skating skeleton"
-        className="absolute bottom-56 left-1/2 z-10 size-1/2 -translate-x-1/2 transform object-center"
+        className="absolute bottom-10 left-1/2 z-10 size-1/4 -translate-x-1/2 transform object-center md:bottom-28 lg:bottom-56 lg:size-1/2"
       />
     </div>
   );
