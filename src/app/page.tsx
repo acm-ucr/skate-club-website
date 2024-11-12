54be735a3b19c7736fd7281d220184a03aeb8e1d
javi <javi@Javis-MacBook-Pro-3.local>
2024-11-11 18:56:13 -0800

    Format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import SkeletonSkater from "@/components/home/SkeletonSkater";
 
 const Home = () => {
   return (
-    <div className="my-10 mt-40 flex h-screen w-screen flex-col items-center justify-center">
-      <div className="mt-20">
-        {" "}
-        <SkeletonSkater />{" "}
-      </div>
+    <div className="my-10 mt-40 flex h-screen w-screen flex-col items-center justify-center bottom-0">
+        <SkeletonSkater />
+    </div>
   );
 };
 
