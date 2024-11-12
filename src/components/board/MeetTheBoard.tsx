d6a50f1d1eaf3ee0fe9bffe634358fbb5ae9cec2
javi <javi@Javis-MacBook-Pro-3.local>
2024-11-11 18:15:05 -0800

    Added Board Photos

--- src/components/board/MeetTheBoard.tsx
+++ src/components/board/MeetTheBoard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const MeetTheBoard = () => {
   return (
     <div className="flex w-full justify-center">
-      <h1 className="font-orbitron text-8xl text-white">Meet The Board</h1>
+      <h1 className="font-banco text-8xl text-white">Meet The Board</h1>
     </div>
   );
 };
