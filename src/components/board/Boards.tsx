8613fbdf6497342a05d46bfc3bafd015251c84db
Javier Becerril <javi@Javis-M4.local>
2024-11-13 02:27:49 -0800

    Add Files for Issues

---
+++ src/components/board/Boards.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import BoardCard from "./BoardCard";
+// import {BoardMembers} from "@/src/data/BoardMembers.js"
+
+const Boards = () => {
+  return (
+    <div className="">
+        Map Here 
+    </div>
+  );
+};
+
+export default Boards;
---
+++ src/data/BoardMembers.js
@@ -0,0 +1,203 @@
+import Alex from "@/public/board/ALEX.webp"
+import Ben from "@/public/board/BEN.webp"
+import Charles from "@/public/board/CHARLESS.webp";
+import Evan from "@/public/board/EVAN.webp";
+import Grace from "@/public/board/GRACE.webp";
+import Griffin from "@/public/board/GRIFFIN.webp";
+import Harry from "@/public/board/HARRY.webp";
+import Jeremey from "@/public/board/JERM.webp";
+import Madi from "@/public/board/MADI.webp";
+import Nico from "@/public/board/NICO.webp";
+import Owen from "@/public/board/ulises.webp";
+import Phil from "@/public/board/PHIL.webp";
+import Radwan from "@/public/board/RADWAN.webp";
+import Savian from "@/public/board/SAVIAN.webp";
+
+export const Members = [
+  {
+    image: Alex,
+
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+
+    key: 1,
+  },
+  {
+    image: Ben,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+
+    key: 2,
+  },
+  {
+    image: Charles,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+
+    key: 3,
+  },
+  {
+    image: Evan,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 4,
+  },
+  {
+    image: Grace,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 5,
+  },
+  {
+    image: Griffin,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 6,
+  },
+  {
+    image: Harry,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 7,
+  },
+  {
+    image: Jeremey,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 8,
+  },
+  {
+    image: Madi,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 9,
+  },
+  {
+    image: Nico,
+   // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 10,
+  },
+  {
+    image: Owen,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 11,
+  },
+  {
+    image: Phil,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 12,
+  },
+  {
+    image: Radwan,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 13,
+  },
+  {
+    image: Savian,
+    // Fill in the rest here
+
+    // position: string;
+    // name: string;
+    // year: number;
+    // major: string;
+    // yearsSkating: number;
+    // favoriteSkater: string;
+    // boardImage: StaticImageData;
+    key: 14,
+  },
+];
