1fe57dbd7780a5345aa05dea22bdb82aa8af1467
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-11-15 14:15:41 -0800

    Adjusted not-found page. Created button that takes user backs to home. Used Framer motion to animate skaters skating accross the screen on top of the Navbar

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,67 @@
-const NotFound = () => {
-  return <div>404 Page Not Found</div>;
-};
+"use client";
+import { motion } from "framer-motion";
 
-export default NotFound;
+export default function NotFound() {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-black p-4 text-center font-banco text-white">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="mb-4 text-6xl font-bold"
+        >
+          404
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          className="mb-16 text-xl"
+        >
+          Oops! Page not found
+        </motion.p>
+        <motion.a
+          href="/"
+          initial={{ opacity: 0, scale: 1.1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            delay: 1.4,
+            duration: 0.5,
+            type: "spring",
+            stiffness: 250,
+          }}
+          className="rounded-full bg-white px-6 py-3 font-semibold text-black"
+        >
+          Go back home
+        </motion.a>
+      </motion.div>
+
+      <div>
+        <motion.img
+          src="/assets/Ghost.svg"
+          alt="Ghost"
+          initial={{ x: "-100vw" }}
+          animate={{ x: "100vw" }}
+          transition={{ duration: 8, repeat: Infinity, ease: "linear" }}
+          style={{ rotate: -15, y: 20 }}
+          className="absolute bottom-0 h-64 w-64"
+        />
+
+        <motion.img
+          src="/assets/Skater.svg"
+          alt="Skater"
+          initial={{ x: "100vw" }}
+          animate={{ x: "-100vw" }}
+          style={{ rotate: -65, y: 45 }}
+          transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
+          className="absolute bottom-0 h-64 w-64"
+        />
+      </div>
+    </div>
+  );
+}
