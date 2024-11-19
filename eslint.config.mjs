d8dc9420bd16af24a2cc02b6c5a953b842673c27
Javier Becerril <javi@Javis-M4.local>
2024-11-19 15:17:50 -0800

    Eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,11 @@ export default [
   {
     rules: {
       "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        { allowShortCircuit: true, allowTernary: true },
+      ],
     },
+    
   },
 ];
