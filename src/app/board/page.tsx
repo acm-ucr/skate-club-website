204b9eb0ebf4f875df19d718dbb8e3386da67047
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-11-21 11:27:51 -0800

    Added Droopy svg. Added responsive spacing between ramp and page contents bc tailwind's pt-values aren't high enough. Added Responsiveness to BoardsSkater.tsx

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -4,9 +4,10 @@ import BoardsSkater from "@/components/board/BoardsSkater";
 
 const page = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center gap-10 bg-black">
+    <div className="flex h-full w-full flex-col items-center justify-between gap-10 bg-black">
       <MeetTheBoard />
       <Boards />
+      <div className="pt-32 lg:h-96 lg:pt-[40%]"></div>
       <BoardsSkater />
     </div>
   );
--- src/components/board/BoardsSkater.tsx
+++ src/components/board/BoardsSkater.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import Ramp from "@/public/assets/GalleryRamp.svg";
+import Droopy from "@/public/assets/Droopy.svg";
 
 export default function BoardsSkater() {
   return (
-    <div className="flex text-3xl font-bold text-white">
-      Boards Skater Drawing Here
+    <div className="relative w-full">
+      <Image
+        src={Ramp}
+        alt="skating ramp"
+        className="absolute bottom-0 w-full"
+      />
+      <Image
+        src={Droopy}
+        alt="skating ghost"
+        className="absolute bottom-0 w-1/4 -translate-y-2/3 translate-x-2/3 transform lg:w-[10%] lg:-translate-y-[175%] lg:translate-x-[225%]"
+      />
     </div>
   );
 }
