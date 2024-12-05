3f5802f0ffe34a8ecd9299d030e60f4aef9ba712
javi <javi@ucr-secure-43-10-13-220-53.wnet.ucr.edu>
2024-12-05 11:50:01 -0800

    Update About US Page

--- src/components/about/AboutUsText.tsx
+++ src/components/about/AboutUsText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutUsText = () => {
   return (
-    <div className="mt-6 flex w-full place-content-center text-3xl">
+    <div className="mb-[10%] flex flex-col w-full items-center gap-3 text:2xl md:text-3xl ">
       <p className="w-3/4 bg-black text-center font-balooTamma font-bold text-white md:w-2/3">
         Skateboarding club at UC Riverside is an official, university-recognized
         student organization that has existed for nearly 10 years. Its main
@@ -30,6 +30,40 @@ const AboutUsText = () => {
         academic excellence while being involved in the alluring culture of
         skateboarding!
       </p>
+
+      <div className="flex flex-col gap-y-12 md:gap-y-20 w-3/4 h-full mt-[5%]">
+
+        <div className="flex flex-col gap-y-12 md:flex-row md:gap-x-20">
+          <div className="flex-1">
+            <iframe
+              src="https://www.youtube.com/embed/ZCsvgNYj8KE"
+              className="aspect-video w-full h-full animate-jump-in rounded border-[3px] shadow-[22px_22px_0px_0px_#F1F5F9] duration-200"
+            />
+          </div>
+          <div className="flex-1">
+            <iframe
+              src="https://www.youtube.com/embed/vZQ2LUvnem0"
+              className="aspect-video w-full h-full animate-jump-in rounded border-[3px] shadow-[22px_22px_0px_0px_#F1F5F9] duration-200"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-y-12 md:flex-row md:gap-x-20">
+          <div className="flex-1">
+            <iframe
+              src="https://www.youtube.com/embed/1ot8tbXaiTY"
+              className="aspect-video w-full h-full animate-jump-in rounded border-[3px] shadow-[22px_22px_0px_0px_#F1F5F9] duration-200"
+            />
+          </div>
+          <div className="flex-1">
+            <iframe
+              src="https://www.youtube.com/embed/eAQlP_wmF3I"
+              className="aspect-video w-full h-full animate-jump-in rounded border-[3px] shadow-[22px_22px_0px_0px_#F1F5F9] duration-200"
+            />
+          </div>
+        </div>
+    </div>
+
     </div>
   );
 };
