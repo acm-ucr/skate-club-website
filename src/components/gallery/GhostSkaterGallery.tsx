2c5de465295157cce10a20eed67827d275f97824
javi <javi@Javis-M4.local>
2024-12-27 19:20:24 -0800

    Responsidy Gallery

--- src/components/gallery/GhostSkaterGallery.tsx
+++ src/components/gallery/GhostSkaterGallery.tsx
@@ -5,13 +5,26 @@ import GalleryRamp from "@/public/assets/GalleryRamp.svg";
 
 const GhostSkaterGallery = () => {
   return (
-    <div className="flex w-full flex-col-reverse items-center">
-      <Image src={GalleryRamp} alt="skating ramp" className="-mt-40 w-full" />
-      <div className="flex">
-        <Image src={Ghost} alt="skating ghost" className="size-full" />
-      </div>
+    <div className="flex flex-col items-center justify-center pt-28 md:p-0 lg:pt-28">
+      <Image
+        src={Ghost}
+        alt="skating ghost"
+        className="bottom-10 z-10 size-[25%] -rotate-12 translate-y-[30%] transform object-center lg:-rotate-12 lg:bottom-56 lg:size-[15%] lg:translate-y-[47%]"
+      />
+      <Image
+        src={GalleryRamp}
+        alt="skating ramp"
+        className="bottom-0 w-full translate-y-[0%] object-center lg:translate-y-[0%]"
+      />
     </div>
   );
 };
 
 export default GhostSkaterGallery;
+
+
+
+
+
+
+
